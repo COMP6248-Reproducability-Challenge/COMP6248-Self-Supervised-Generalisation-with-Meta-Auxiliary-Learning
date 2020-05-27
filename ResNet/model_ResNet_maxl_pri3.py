135a2d380d5ab927b680e3e55e1e61c585fd80f7
Gabriel Pan <53709439+gabrielpan147@users.noreply.github.com>
2020-05-27 16:26:03 +0100

    Update model_ResNet_maxl_pri3.py

--- ResNet/model_ResNet_maxl_pri3.py
+++ ResNet/model_ResNet_maxl_pri3.py
@@ -302,11 +302,7 @@ class ResNet(nn.Module):
                                                                  convnum=stride_num,finepara=4)],training=True)
                 counter += 1
       
-        #
-        #     #TODO:我他妈该怎么形容我的需求
-        #         #TODO：研究ResNet的网络结构 搞清楚这个参数到底尼玛以什么形式出现
-        #         #TODO:搞清楚了 舒服了 明天按照网络参数输出来写这个传到结构 然后直接把姐姐的标签分类借用过来 改吧改吧得一个CISIC的版本
-        #         #TODO：不知道不同参数的网络用不用改代码 希望不用
+
         if index==1:
             self.lay1_out_net=net
         elif index ==2:
